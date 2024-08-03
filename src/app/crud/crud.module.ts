43e7881607a3c7c502aeda01ae395ee4ce3d9093
Reno2512 <robertemmanuelmamadousagne@esp.sn>
2024-08-03 16:00:23 +0000

    interface creer

--- src/app/crud/crud.module.ts
+++ src/app/crud/crud.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CrudRoutingModule } from './crud-routing.module';
+import {AgGridModule} from "ag-grid-angular";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CrudRoutingModule
+    CrudRoutingModule,
+    AgGridModule
   ]
 })
 export class CrudModule { }
--- src/app/crud/vente-list/vente-list.component.ts
+++ src/app/crud/vente-list/vente-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ColDef} from "ag-grid-community";
+import {AgGridAngular} from "ag-grid-angular";
 
 @Component({
   selector: 'app-vente-list',
   standalone: true,
-  imports: [],
+  imports: [
+    AgGridAngular
+  ],
   templateUrl: './vente-list.component.html',
   styleUrl: './vente-list.component.scss'
 })
-export class VenteListComponent {
+export class VenteListComponent implements OnInit{
+  // Row Data: The data to be displayed.
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+  frameworkComponent = {
+    customCellRenderer: VenteListComponent
+  };
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ];
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 
 }
